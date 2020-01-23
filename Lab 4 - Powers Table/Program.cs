8e7899693ff27cd86c8f5425e14b0760c2068f0c
Michael <Michael@acdmail.com>
2020-01-23 17:02:56 -0500

    base power table but without any vailation

--- Lab 4 - Powers Table/Program.cs
+++ Lab 4 - Powers Table/Program.cs
@@ -6,7 +6,25 @@ namespace Lab_4___Powers_Table
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey, let's make a powers table! Enter an integer");
+
+            int userNum1 = int.Parse(Console.ReadLine());
+            CalculateTable(userNum1);
+
+        }
+        public static void CalculateTable(int num1)
+        {
+            double square = 1;
+            double cube = 1;
+            Console.WriteLine(string.Format("{0,-10}    {1,-10}    {2,-10}", "Number", "Square", "Cubed"));
+            Console.WriteLine(string.Format("{0,-10}    {0,-10}    {0,-10}", "========="));
+
+            for (int i = 1; i <= num1; i++)
+            {
+                square = Math.Pow(i, 2);
+                cube = Math.Pow(i, 3);
+                Console.WriteLine(string.Format("{0,-10} |  {1,-10} |  {2,-10}", i, square, cube));
+            }
         }
     }
 }
