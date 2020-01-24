830374e11fa6f8dedd5b327b3e63c99f29e3e90f
Michael <Michael@acdmail.com>
2020-01-23 19:30:05 -0500

    Added validation and y/n continue

--- Lab 4 - Powers Table/Program.cs
+++ Lab 4 - Powers Table/Program.cs
@@ -6,24 +6,44 @@ namespace Lab_4___Powers_Table
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hey, let's make a powers table! Enter an integer");
-
-            int userNum1 = int.Parse(Console.ReadLine());
-            CalculateTable(userNum1);
+            Console.Write("Hey, let's make a powers table!\n");
+            
+            CalculateTable();
 
         }
-        public static void CalculateTable(int num1)
+        public static void CalculateTable()
         {
-            double square = 1;
-            double cube = 1;
-            Console.WriteLine(string.Format("{0,-10}    {1,-10}    {2,-10}", "Number", "Square", "Cubed"));
-            Console.WriteLine(string.Format("{0,-10}    {0,-10}    {0,-10}", "========="));
-
-            for (int i = 1; i <= num1; i++)
+            
+            char userContinue = 'y';
+            while (userContinue == 'y')
             {
-                square = Math.Pow(i, 2);
-                cube = Math.Pow(i, 3);
-                Console.WriteLine(string.Format("{0,-10} |  {1,-10} |  {2,-10}", i, square, cube));
+                Console.Write("\nEnter an integer: ");
+                int userNum1 = int.Parse(Console.ReadLine());
+                if (userNum1 >= 0)
+                {
+                    double square, cube;
+
+                    Console.WriteLine(string.Format("\n{0,-10}    {1,-10}    {2,-10}", "Number", "Square", "Cubed"));
+                    Console.WriteLine(string.Format("{0,-10}    {0,-10}    {0,-10}", "========="));
+
+                    for (int i = 1; i <= userNum1; i++)
+                    {
+                        square = Math.Pow(i, 2);
+                        cube = Math.Pow(i, 3);
+                        Console.WriteLine(string.Format("{0,-10} |  {1,-10} |  {2,-10}", i, square, cube));
+                    }
+                    userContinue = ' ';
+                    while (userContinue != 'y' && userContinue != 'n')
+                    {
+                        Console.WriteLine("\nWould you like to continue? y/n?");
+                        userContinue = Console.ReadLine()[0];
+
+                        if (userContinue == 'n')
+                        {
+                            Console.WriteLine("OKBAI!!");
+                        }
+                    }
+                }
             }
         }
     }
